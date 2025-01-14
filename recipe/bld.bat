7543dca7f8b7a0f3fb96c251ece07fed58be887e
Daniel Nachun <daniel.nachun@gmail.com>
2025-01-14 01:36:34 -0800

    fix build

--- recipe/bld.bat
+++ recipe/bld.bat
@@ -2,7 +2,7 @@
 
 go build -buildmode=pie -trimpath -o=%LIBRARY_PREFIX%\bin\%PKG_NAME%.exe ^
     -ldflags="-s -X github.com/homeport/dyff/internal/cmd.version=%PKG_VERSION%" .\cmd\dyff || goto :error
-go-licenses save . --save_path=license-files || goto :error
+go-licenses save .\cmd\dyff --save_path=license-files || goto :error
 
 goto :EOF
 
