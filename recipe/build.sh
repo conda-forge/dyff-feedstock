09d9f2b307755fe3d0f7325c091692a8533a9ef4
Daniel Nachun <daniel.nachun@gmail.com>
2025-01-14 01:20:07 -0800

    enable osx-arm64

--- recipe/build.sh
+++ recipe/build.sh
@@ -3,5 +3,5 @@
 set -o xtrace -o nounset -o pipefail -o errexit
 
 go build -buildmode=pie -trimpath -o=${PREFIX}/bin/${PKG_NAME} \
-    -ldflags="-s -w -X github.com/homeport/dyff/internal/cmd.version={{ version }}" ./cmd/dyff
+    -ldflags="-s -w -X github.com/homeport/dyff/internal/cmd.version=${PKG_VERSION}" ./cmd/dyff
 go-licenses save . --save_path=license-files
