dbdd8c01a80192853d97d8002a9d864641ead1ff
Daniel Nachun <daniel.nachun@gmail.com>
2025-11-25 19:23:47 -0800

    use rattler

--- recipe/build.bat
+++ recipe/build.bat
@@ -1,10 +1,11 @@
 @echo on
+@setlocal EnabledDelayedExpansion
 
-go build -buildmode=pie -trimpath -o=%LIBRARY_PREFIX%\bin\%PKG_NAME%.exe ^
+go build -o=%LIBRARY_PREFIX%\bin\%PKG_NAME%.exe ^
     -ldflags="-s -X github.com/homeport/dyff/internal/cmd.version=%PKG_VERSION%" .\cmd\dyff || goto :error
 go-licenses save .\cmd\dyff --save_path=license-files || goto :error
 
-goto :EOF
+goto :eof
 
 :error
 echo Failed with error #%errorlevel%.
